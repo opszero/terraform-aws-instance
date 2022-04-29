8d0be6075a9159852ff2b73b3a34f4ccce75e7e0
Abhi Yerra <abhi@berkeley.edu>
2022-04-29 13:57:10 -0700

    Added vastion fixes

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ssm_parameter" "ubuntu" {
 }
 
 resource "aws_eip" "this" {
-  instance = aws_instance.bastion.id
+  instance = aws_instance.this.id
   vpc      = true
   tags     = var.tags
 }
@@ -28,14 +28,14 @@ resource "aws_security_group_rule" "this" {
   cidr_blocks       = [each.key]
   from_port         = 22
   protocol          = "tcp"
-  security_group_id = aws_security_group.bastion.id
+  security_group_id = aws_security_group.this.id
   to_port           = 22
   type              = "ingress"
 }
 
 resource "aws_instance" "this" {
   ami           = data.aws_ssm_parameter.ubuntu.value
-  instance_type = var.bastion_instance_type
+  instance_type = var.instance_type
 
   associate_public_ip_address = true
   subnet_id                   = var.subnet_id
