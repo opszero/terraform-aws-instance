47ab378742bddcfd531344ad6594eb7a9d1420e8
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2022-05-14 01:31:06 +0630

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,31 @@
+data "template_file" "keys" {
+  vars = {
+    ssh_key = var.ssh_key
+    userdata = var.user_data
+  }
+
+  template = file("${path.module}/keys.tpl")
+}
+
+data "template_file" "ec2_connect" {
+  count = var.ec2_connect_installed ? 1 : 0
+
+  template = file("${path.module}/ec2_connect.sh")
+}
+
+data "template_cloudinit_config" "config" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = data.template_file.keys.rendered
+  }
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = data.template_file.ec2_connect[0].rendered
+  }
+}
+
+data "aws_caller_identity" "current" {}
